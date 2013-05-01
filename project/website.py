ce3459eccbf1f725a8d686655f27c1384f0561fd
user <user@xubuntu-12.10-64>
2013-05-01 18:07:28 +0200

    Updated website Minor fix in DataBuilder

--- project/website.py
+++ project/website.py
@@ -2,11 +2,25 @@
 class Website():
 
     def __init__(self, dat_file):
-        self.url = ""
+        self.url = ''
         self.headers= {}
+
         self.alexa_rank = 0
-        self.response_time = 0
-        self.html = ""
+        self.alexa_rank_dk = 0
+        self.google_page_rank = 0
+
+        self.alexa_load_time = 0
+        self.measured_response_time = 0
+
+        self.alexa_adult_content = False
+        self.alexa_title = ''
+        self.alexa_description = ''
+        alexa_lang = ''
+        alexa_links_in = 0
+
+        self.robots = ''
+
+        self.html = ''
         self.dat_file = dat_file
         self.parse()
 
@@ -16,10 +30,12 @@ class Website():
             try:
                 if line.startswith('### URL:'):
                     self.url = line.strip('### URL:').strip()
-                elif line.startswith('### RANK:'):
-                    self.alexa_rank = int(line.strip('### RANK:').strip())
-                elif line.startswith('### TIME:'):
-                    self.response_time = float(line.strip('### TIME:').strip())
+                elif line.startswith('### ALEXA_RANK:'):
+                    self.alexa_rank = int(line.strip('### ALEXA_RANK:').strip())
+                elif line.startswith('### ALEXA_RANK_DK:'):
+                    self.alexa_rank_dk = int(line.strip('### ALEXA_RANK_DK:').strip())
+                elif line.startswith('### MEASURED_TIME:'):
+                    self.measured_response_time = float(line.strip('### MEASURED_TIME:').strip())
                 elif line.startswith('### HEADERS:'):
                     header_line = self.dat_file.readline()
                     while not header_line.isspace():
@@ -29,9 +45,28 @@ class Website():
                         header_line = self.dat_file.readline()
                 elif line.startswith('### HTML:'):
                     html_line = self.dat_file.readline()
-                    while not html_line.isspace():
+                    while not html_line.startswith('###'):
                         self.html += html_line
                         html_line = self.dat_file.readline()
+                elif line.startswith('### ROBOTS:'):
+                    robots_line = self.dat_file.readline()
+                    while not html_line.startswith('###'):
+                        self.robots += robots_line
+                        robots_line = self.dat_file.readline()
+                elif line.startswith('### ALEXA_LOAD_TIME'):
+                    self.alexa_load_time = int(line.strip('### ALEXA_LOAD_TIME:').strip())
+                elif line.startswith('### GOOGLE_PAGE_RANK'):
+                    self.google_page_rank = int(line.strip('### GOOGLE_PAGE_RANK:').strip())
+                elif line.startswith('### ALEXA_HAS_ADULT_CONTENT'):
+                    self.alexa_adult_content = bool(line.strip('### ALEXA_HAS_ADULT_CONTENT:').strip())
+                elif line.startswith('### ALEXA_TITLE:'):
+                    self.alexa_title = line.strip('### ALEXA_TITLE:').strip()
+                elif line.startswith('### ALEXA_DESCRIPTION:'):
+                    self.alexa_description = line.strip('### ALEXA_DESCRIPTION:').strip()
+                elif line.startswith('### ALEXA_LANG:'):
+                    self.alexa_lang = line.strip('### ALEXA_LANG:').strip()
+                elif line.startswith('### ALEXA_LINKS_IN'):
+                    self.alexa_links_in = int(line.strip('### ALEXA_LINKS_IN:').strip())
                 line = self.dat_file.readline()
 
             except Exception, e:
--- top_sites/data_builder.py
+++ top_sites/data_builder.py
@@ -14,7 +14,11 @@ class DataBuilder:
     def generate(self):
         contents = u'### START ###\n\n'
         for key, value in self.singleline_attributes.iteritems():
+            if isinstance(value, str):
+                value = value.decode('utf-8')
             contents += u'### %s: %s\n\n' % (key, value)
         for key, value in self.multiline_attributes.iteritems():
+            if isinstance(value, str):
+                value = value.decode('utf-8')
             contents += u'### %s:\n%s\n\n' % (key, value)
         return contents + '### STOP ###'
