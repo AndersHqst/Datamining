dc4d7c84ecb64d13cf3ce6203c1adb1b5800a86a
AndersHqst <anders.host.kjaergaard@gmail.com>
2013-05-01 19:39:30 +0200

    added cms scanner to init

--- project/scanners/__init__.py
+++ project/scanners/__init__.py
@@ -3,3 +3,4 @@ from img_count_scanner import image_count_scanner
 from external_links_scanner import external_links_scanner
 from internal_links_scanner import internal_links_scanner
 from title_scanner import title_scanner
+from cms_scanner import cms_scanner
