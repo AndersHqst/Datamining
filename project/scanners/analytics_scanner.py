c178e28ad13ce96d471444100fa90377343ec7f7
AndersHqst <anders.host.kjaergaard@gmail.com>
2013-05-08 13:00:19 +0200

    Updated analytics scanner to beautifulsoup

--- project/scanners/analytics_scanner.py
+++ project/scanners/analytics_scanner.py
@@ -1,47 +1,13 @@
-from HTMLParser import HTMLParser
-
-"""True is a website uses google analytics"""
-
-class AnalyticsStopException(Exception):
-    def __init__(self, *args, **kwargs):
-        super(Exception, self).__init__(*args, **kwargs)
-
-class AnalyticsHTMLParser(HTMLParser):
-
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.has_analytics = False
-        self.is_script = False
-
-    def handle_starttag(self, tag, attrs):
-        self.is_script = False
-        if tag == 'script':
-            self.is_script = True
-
-
-    def handle_endtag(self, tag):
-        self.is_script = False
-
-
-    def handle_data(self, data):
-        if self.is_script == True:
-            if 'google-analytics.com/ga.js' in data:
-                self.has_analytics = True
-                raise AnalyticsStopException()
-
-
+"""True is a website using google analytics"""
 
 def bins():
     return [0, 1]
 
 def analytics_scanner(website):
-    parser = AnalyticsHTMLParser()
-    try:
-        parser.feed(website.html)
-    except Exception as e:
-        if not isinstance(e, AnalyticsStopException):
-            print 'AnalyticsScanner Exception: ', e
-            print 'site: ', website.url
-            if hasattr(e, 'read'):
-                print e.read()
-    return ('has_analytics', int(parser.has_analytics))
+    has_analytics = False
+    scripts = website.soup.find_all('script')
+    for script in scripts:
+        if 'google-analytics.com/ga.js' in script.get_text():
+            has_analytics = True
+            break
+    return ('has_analytics', int(has_analytics))
