fa68de588c5ae692fd54ba179ee66dfe712b7739
AndersHqst <anders.host.kjaergaard@gmail.com>
2013-05-08 15:30:50 +0200

    Rewrite of cms scanner to use soup

--- project/create_dataset.py
+++ project/create_dataset.py
@@ -7,42 +7,42 @@ from website_analyzer import analyze
 from scanners import *
 
 # Settings
-website_dir = 'top_sites_2'
+website_dir = 'top_sites'
 
 scanners = [
-    url_scanner,
-    image_count_scanner,
-    external_links_scanner,
-    internal_links_scanner,
-    title_scanner,
-    cms_scanner,
-    description_scanner,
-    keyword_scanner,
-    alexa_rank_scanner,
-    alexa_rank_dk_scanner,
-    alexa_load_time_scanner,
-    alexa_links_ins_scanner,
-    alexa_lang_scanner,
-    server_scanner,
-    analytics_scanner,
-    url_scanner,
-    image_count_scanner,
-    external_links_scanner,
-    internal_links_scanner,
-    title_scanner,
-    cms_scanner,
-    description_scanner,
-    keyword_scanner,
-    alexa_rank_scanner,
-    alexa_rank_dk_scanner,
-    alexa_load_time_scanner,
-    alexa_links_ins_scanner,
-    alexa_lang_scanner,
-    html5_scanner,
-    html5_tag_scanner,
-    xhtml_scanner,
-    twitter_share_scanner, 
-    facebook_share_scanner
+    # url_scanner,
+    # image_count_scanner,
+    # external_links_scanner,
+    # internal_links_scanner,
+    # title_scanner,
+    # cms_scanner,
+    # description_scanner,
+    # keyword_scanner,
+    # alexa_rank_scanner,
+    # alexa_rank_dk_scanner,
+    # alexa_load_time_scanner,
+    # alexa_links_ins_scanner,
+    # alexa_lang_scanner,
+    # server_scanner,
+    # analytics_scanner
+    # url_scanner,
+    # image_count_scanner,
+    # external_links_scanner,
+    # internal_links_scanner,
+    # title_scanner,
+    cms_scanner
+    # description_scanner,
+    # keyword_scanner,
+    # alexa_rank_scanner,
+    # alexa_rank_dk_scanner,
+    # alexa_load_time_scanner,
+    # alexa_links_ins_scanner,
+    # alexa_lang_scanner,
+    # html5_scanner,
+    # html5_tag_scanner,
+    # xhtml_scanner,
+    # twitter_share_scanner,
+    # facebook_share_scanner
 ]
 
 # Get all websites
@@ -51,11 +51,11 @@ websites = []
 
 print 'Files loaded: ', len(files_names)
 
-for fn in files_names:
+for fn in files_names[:100]:
     with open(fn) as f:
         website = Website(f)
         websites.append(website)
-        # print 'Parsed: %s' % website.url
+        print 'Parsed: %s' % website.url
 
 print 'Websites parsed'
 
@@ -64,10 +64,10 @@ attribute_rows = []
 for website in websites:
     attributes = analyze(website, scanners)
     attribute_rows.append(attributes)
-    # print 'Analyzed: %s' % website.url
+    print 'Analyzed: %s' % website.url
 
-print 'attr rows. has analytics'
-print sum(a['has_analytics'] == 1 for a in attribute_rows)
+print 'attr rows. has cms'
+print attribute_rows
 
 # Write to CSV
 writer = CsvWriter(attribute_rows, separator=',', include_header=True, surround_symbol='')
